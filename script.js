3da7a1dd72baa4fe7416dcf38453ecad57a0d472
LuizVCM <luizvcmluizvcm@gmail.com>
2025-06-25 20:45:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("btn");
+const carregar = document.getElementById("carregar");
+
+const iniciar = document.getElementById("iniciar");
+const classe1 = document.getElementById("classe1");
+
+iniciar.addEventListener("click", ()=>{
+    classe1.style.animation = 'auto 2s forwards';
+    iniciar.style.display = 'none';
+
+});
